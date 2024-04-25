7931ac624577edb612ec67350d1dcd23635e5d3b
phoenixx11 <phoenixxss.11@gmail.com>
2024-04-25 14:24:41 +0530

    root

--- vuejs-search-meals/vite.config.js
+++ vuejs-search-meals/vite.config.js
@@ -3,6 +3,11 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    rollupOptions: {
+      input: './index.html', 
+    },
+  },
   server: {
     port: 10000, 
   },
